4d36f56268a7c1bfc0c0fe6ec98445a9cfa9bbb3
Chuong <minhchuong24052001@gmail.com>
2021-11-20 19:46:56 +0700

    hello

---
+++ trangchu.js
@@ -0,0 +1,6 @@
+function menuDropdown() {
+    document.querySelector(".menu-hover").classList.toggle("hienthi");
+}
+function sanphamDropdown() {
+    document.querySelector(".san-pham-hover").classList.toggle("hienthi");
+}
